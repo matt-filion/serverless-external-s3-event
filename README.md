ebcbdb01c477ea37eb02e52dd1997d66af12e310
matt_ <matt.filion@gmail.com>
2016-09-04 10:52:04 -0700

    Initial commit of working plugin.

---
+++ README.md
@@ -0,0 +1,93 @@
+# Why?
+Overcomes the CloudFormation limitation on attaching an event to an uncontrolled bucket, for Serverless.com 1.0+. See [this stackoverflow issue](http://serverfault.com/questions/610788/using-cloudformation-with-an-existing-s3-bucket) for more information.
+
+# What?
+Attach an S3 event to a function defined within your Serverless.com 1.0+ service. Executed via ```sls s3deploy```.
+
+# How?
+
+**NPM dependency**
+_Looking to eliminate this step, as it will place the dependency within your deployed code._
+```
+> npm install serverless-external-s3-event
+
+**Declare the plugin in your serverless.yml**
+```serverless.yml
+
+plugins:
+ - serverless-external-s3-event
+
+```
+
+**Give your deploy permission to access the bucket.**
+The BUCKET_NAME variable within provider.iamRoleStatements.Resource.Fn::Join needs to be replaced with the name of the bucket you want to attach your event(s) to.  If there are multiple buckets you want to attach events to add a new item for each bucket.
+
+```serverless.yml
+
+provider:
+  name: aws
+  runtime: nodejs4.3
+  iamRoleStatements:
+    -  Effect: "Allow"
+       Action:
+         - "s3:ListBucket"
+         - "s3:PutObject"
+       Resource:
+         Fn::Join:
+           - ""
+           - - "arn:aws:s3:::"
+             - "Ref" : "ServerlessDeploymentBucket"
+    -  Effect: "Allow"
+       Action:
+         - "s3:PutBucketNotification"
+       Resource:
+         Fn::Join:
+           - ""
+           - - "arn:aws:s3:::BUCKET_NAME" 
+           - - "arn:aws:s3:::BUCKET_OTHERNAME" 
+```
+
+**Attach an event to your target function.**
+Add an -existingS3 event definition under 'events' of your function declaration. The bucketEvents value is optional. If omitted it will default to a single entry for "s3:ObjectCreated:*".
+
+```serverless.yml
+
+functions:
+  someFunction:
+    handler: index.handler
+    timeout: 60
+    events:
+      - existingS3:
+          bucket: BUCKET_NAME
+          bucketEvents: 
+            - s3:ObjectCreated:*
+```
+
+**Run the command.**
+_I could not figure out how to hook into the existing deploy behaviors built into Serverless.com's deploy command. So as a result you have to run a separate command AFTER you do ```sls deploy```._
+
+```
+> sls deploy
+Serverless: Zipping service...
+Serverless: Uploading CloudFormation file to S3...
+Serverless: Removing old service versions...
+Serverless: Uploading .zip file to S3...
+Serverless: Updating Stack...
+Serverless: Checking stack update progress...
+..
+Serverless: Deployment successful!
+
+Service Information
+service: service-name
+stage: stage
+region: region
+endpoints:
+  None
+functions:
+  someFunction: arn:aws:lambda:region:accountid:function:service-name-stage-someFunction
+
+> sls s3deploy
+Attaching event(s) to: someFunction
+Done.
+
+```
---
+++ index.js
@@ -0,0 +1,116 @@
+'use strict';
+
+class Deploy {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options    = options;
+    this.commands   = {
+      s3deploy: {
+        lifecycleEvents: [
+          'events'
+        ]
+      },
+    };
+    this.hooks = {
+      'after:s3deploy:events': this.afterDeployFunctions.bind(this)
+    };
+  }
+
+  afterDeployFunctions() {
+
+    /*
+     * TODO
+     *  - Event Filters
+     *  - 
+     */
+    const cliOptions          = this.serverless.pluginManager.cliOptions;
+    const sdk                 = this.serverless.pluginManager.plugins.find( item => item.constructor.name === 'AwsDeploy').sdk;
+    const AWS                 = sdk.sdk;
+    const credentials         = sdk.getCredentials(cliOptions.stage,cliOptions.region);
+    const S3                  = new AWS.S3(credentials);
+    const CloudFormation      = new AWS.CloudFormation(credentials);
+    const bucketNotifications = this.serverless.service.getAllFunctions()
+      .map(name => this.serverless.service.getFunction(name) )
+      /*
+       * Create a LambdaFunctionConfigurations for existingS3 configuration
+       *  on each functionObj.
+       */
+      .map(functionObj => this.getLambdaFunctionConfigurationsFromFunction(functionObj) )
+      /*
+       * Flatten the results
+       */
+      .reduce( (accumulator,current) => accumulator = accumulator.concat(current) , [])
+      /*
+       * Organize the resulting configurations so that all events for the
+       *  same bucket are together.
+       */
+      .reduce( (accumulator,current) => {
+        let bucketLambdaConfigs = accumulator.find( eventConfig => eventConfig.name === current.bucket );
+        if(!bucketLambdaConfigs) {
+          bucketLambdaConfigs = { bucket:current.bucket, configs:[] };
+          accumulator.push(bucketLambdaConfigs);
+        }
+        bucketLambdaConfigs.configs.push(current.config);
+        return accumulator;
+      }, [])
+      /*
+       * Create a bucket configuration as AWS needs it for each
+       *  bucket.
+       */
+      .map(bucketConfig=> {
+        return {
+          Bucket:bucketConfig.bucket,
+          NotificationConfiguration: {
+            LambdaFunctionConfigurations:bucketConfig.configs
+          }
+        } 
+      })
+
+    /*
+     * Lookup the ARN for each function referenced within the bucket
+     *  events.
+     */
+    return CloudFormation.describeStacks({ StackName: sdk.getStackName(cliOptions.stage) }).promise()
+      .then(results => results.Stacks[0].Outputs)
+      .then(outputs => {
+        bucketNotifications.forEach( notification => {
+          notification.NotificationConfiguration.LambdaFunctionConfigurations.forEach( lambdaFunctionConfiguration => {
+            var output = outputs.find( output => output.OutputValue.endsWith(':function:'+lambdaFunctionConfiguration.LambdaFunctionArn) )
+            lambdaFunctionConfiguration.LambdaFunctionArn = output.OutputValue
+            
+          })
+        })
+        return bucketNotifications;
+      })
+      /*
+       * Attach the events to each bucket.
+       */
+      .then( buckets => {
+        console.log("Attaching event(s) to:",buckets.reduce( (result,bucket) => result += bucket.Bucket + ' ',''));
+        return buckets;
+      })
+      .then( bucketNotifications => Promise.all( bucketNotifications.map(param => S3.putBucketNotificationConfiguration(param).promise() ) ) )
+      .then( () => console.log("Done."))
+    
+  }
+  
+  getLambdaFunctionConfigurationsFromFunction(functionObj) {
+    return functionObj.events
+      .filter(event => event.existingS3)
+      .map(event => {
+        /*
+         * Hoping the ID causes overwriting of an existing configuration.
+         */
+        return {
+          bucket: event.existingS3.bucket,
+          config: {
+            Id: 'trigger--' + functionObj.name + '--when--' + event.existingS3.bucketEvents.join().replace(/[\.\:\*]/g,''), 
+            LambdaFunctionArn: functionObj.name,
+            Events: event.existingS3.bucketEvents  || ['s3:ObjectCreated:*']
+          }
+        }
+      })
+  }
+}
+
+module.exports = Deploy;
